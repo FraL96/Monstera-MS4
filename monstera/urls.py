de2c9a6f741a1c3967f1909c4d334b7fa6cece6b
FraL96 <francesca_lupu@live.it>
2021-09-01 22:55:48 +0000

    Removed blog app for malfunctioning

--- monstera/urls.py
+++ monstera/urls.py
@@ -9,9 +9,6 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -27,5 +24,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('', include('faq.urls')),
-    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
