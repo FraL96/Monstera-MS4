cb451e8cd8e92fe258920f1ad6de3ff1e58074d3
FraL96 <francesca_lupu@live.it>
2021-09-02 01:55:44 +0000

    Added model for comments in blog but not working yet

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='blog'),
     path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
+    path('<slug:slug>/', views.article_detail, name='article_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,39 @@
 from django.views import generic
 from .models import Article
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+
 
 class ArticleList(generic.ListView):
     queryset = Article.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/blog.html'
 
+
 class ArticleDetail(generic.DetailView):
     model = Article
     template_name = 'blog/article_detail.html'
+
+
+def article_detail(request, slug):
+    template_name = 'article_detail.html'
+    article = get_object_or_404(Article, slug=slug)
+    comments = article.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current article to the comment
+            new_comment.article = article
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'article': article,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
