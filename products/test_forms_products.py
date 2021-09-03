22900426f933d16670227dd9d5509fea5a3a1a34
FraL96 <francesca_lupu@live.it>
2021-09-03 05:21:45 +0000

    Cleaned code + test forms for products

---
+++ products/test_forms_products.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from .forms import ProductForm
+
+
+class TestProductForm(TestCase):
+
+    def test_product_form_is_required(self):
+        form = ProductForm({
+            'name': '',
+            'descripton': '',
+            'price': '',
+            })
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertIn('description', form.errors.keys())
+        self.assertIn('price', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+        self.assertEqual(
+            form.errors['description'][0], 'This field is required.')
+        self.assertEqual(form.errors['price'][0], 'This field is required.')
+
+    def test_product_form_is_not_required(self):
+        form = ProductForm({
+            'sku': 'test sku',
+            'category': 'category',
+            'image': 'image',
+            })
+        self.assertTrue(form.is_valid)
